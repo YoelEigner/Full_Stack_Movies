a867a63a7b09c8e239c57f8aba659e058caee943
YoelEigner <35924447+YoelEigner@users.noreply.github.com>
2021-10-21 19:45:52 +0300

    Delete subscriptions directory

--- subscriptions/configs/subscriptionsSchema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const SubscriptionsSchema = new mongoose.Schema({
-  memberId: String,
-  movies: { movieId: String, date: Date }
-});
-
-module.exports = mongoose.model("subscriptions", SubscriptionsSchema);
